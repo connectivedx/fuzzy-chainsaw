c52538932318eca04b661ffeb97f79043eed638a
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-05-13 18:36:23 -0700

    linting task

---
+++ tasks/lint-styles.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const lint = require('../submodules/task-lint-styles').lint;
+const dirs = require('../package.json').directories
+
+gulp.task('lint-styles', lint({
+	src: dirs.components + '/*.css'
+}));
+
+gulp.task('lint-styles-watch', () => {
+	gulp.watch('source/components/**/*.css', ['lint-styles']);
+});
