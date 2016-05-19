a37523524f7e0e2359e10f238348a67346bc699e
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-05-19 11:54:27 -0700

    clarify

--- submodules/task-scripts/tests/fixtures/main.js
+++ submodules/task-scripts/tests/fixtures/main.js
@@ -1,17 +1,17 @@
 import template from './test.hbs';
 import test from 'tape';
 
-const result = template({
-  id: '1234',
-  class: 'volcano',
-  text: 'Big Volcano'
-});
-
 // if this compiles and runs successfully
 // we can *assume* that a bulk of moving
 // parts in the browserify build are
 // operating correctly.
 test('gulp spot-test', t => {
+  const result = template({
+    id: '1234',
+    class: 'volcano',
+    text: 'Big Volcano'
+  });
+
   t.plan(1);
   t.equal(result, '<div id="1234" class="volcano">Big Volcano</div>\n');
 });
