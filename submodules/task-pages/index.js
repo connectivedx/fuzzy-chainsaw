30cf323f39d0ba3b76823f93a9db2e506d9b51dc
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-05-20 11:20:50 -0700

    remove html pretty

--- submodules/task-pages/index.js
+++ submodules/task-pages/index.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const gulp = require('gulp');
 const ext = require('gulp-ext-replace');
-const prettify = require('gulp-prettify');
 const hb = require('gulp-hb');
 
 const getOptions = require('./lib/getOptions');
@@ -13,9 +12,6 @@ module.exports = function(options) {
 	return function() {
 		return gulp.src(opts.src)
 			.pipe(hb(opts.hb))
-		    .pipe(prettify({
-		    	indent_size: 2
-		    }))
 			.pipe(ext('.html'))
 			.pipe(gulp.dest(opts.dest));
 	}
