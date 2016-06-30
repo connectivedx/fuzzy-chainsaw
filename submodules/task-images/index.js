1b63810fa93c3f562cec5acdc9e827463583991b
Dan Manchester <dan@elseloop.com>
2016-06-30 16:25:33 -0700

    Adds first pass at task-images submodule

--- submodules/task-images/index.js
+++ submodules/task-images/index.js
@@ -1,6 +1,16 @@
 const gulp = require('gulp');
-module.exports = function(opts) {
+const gutil = require('gulp-util');
+const imagemin = require('gulp-imagemin');
+const getOptions = require('./lib/getOptions');
+
+module.exports = function(options) {
+	var opts = getOptions(options);
+
 	return function() {
-    return gulp.src(opts.src + '/**/*').pipe(gulp.dest(opts.dest));
-	}
+		return (
+			gulp.src(opts.src)
+				.pipe(gutil.env.type === 'production' ? imagemin() : gutil.noop())
+				.pipe(gulp.dest(opts.dest))
+		);
+	};
 };
---
+++ submodules/task-images/lib/getOptions.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const PluginName = require('../package.json').name
+
+module.exports = function getOptions(options) {
+  if (options === undefined) {
+    throw new Error(PluginName + ': options is not defined');
+  }
+
+  if (options.src === undefined) {
+    throw new Error(PluginName + ': options.src is not defined');
+  } else {
+    if (options.src.indexOf('*') !== -1) {
+      options.src = options.src;
+    } else {
+      options.src = path.join(options.src, '**/*');
+    }
+  }
+
+  if (options.dest === undefined) {
+    throw new Error(PluginName + ': options.dest is not defined');
+  }
+
+  return options;
+};
