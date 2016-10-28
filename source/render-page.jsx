1e2dc8a95b0d7d2449dc1bbdd4bc1e899840c665
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-10-28 10:40:40 -0700

    moved pathing fix to where paths are being generated

--- source/render-page.jsx
+++ source/render-page.jsx
@@ -4,6 +4,6 @@ import Dom from 'react-dom/server';
 
 module.exports = function renderPage(locals, callback) {
 	const path = locals.path.substr(0, locals.path.length - 5);
-	const Page = require('./pages/' + path.replace(/\\/g, '/') + '.jsx').default;
+	const Page = require('./pages/' + path + '.jsx').default;
   callback(null, '<!DOCTYPE html>' + Dom.renderToStaticMarkup(<Page locals={locals} />, locals));
 };
