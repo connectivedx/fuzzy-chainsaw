301087f08685ed452f5aa6201c7d981cd7718a16
Christoph Saxe <csaxe@connectivedx.com>
2016-10-27 23:28:02 -0700

    normalizes path delimiters to Unix style forward slash

--- source/render-page.jsx
+++ source/render-page.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import Dom from 'react-dom/server';
 
 
-
 module.exports = function renderPage(locals, callback) {
 	const path = locals.path.substr(0, locals.path.length - 5);
-	const Page = require('./pages/' + path + '.jsx').default;
+	const Page = require('./pages/' + path.replace(/\\/g, '/') + '.jsx').default;
   callback(null, '<!DOCTYPE html>' + Dom.renderToStaticMarkup(<Page locals={locals} />, locals));
 };
