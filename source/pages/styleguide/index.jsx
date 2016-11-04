578017518a208a10c12c0e96c67b553bba6f11a2
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-11-03 22:00:20 -0700

    missed a file

--- source/pages/styleguide/index.jsx
+++ source/pages/styleguide/index.jsx
@@ -4,7 +4,7 @@ import PageRoot from '../../tags/PageRoot/PageRoot';
 import Rhythm from '../../tags/Rhythm/Rhythm';
 import Heading from '../../tags/Heading/Heading';
 import Wrapper from '../../tags/Wrapper/Wrapper';
-import ComponentList from '../../styleguide/components/ComponentList';
+import LinkList from '../../styleguide/components/LinkList';
 
 export default ({ locals }) => (
 	<PageRoot title="Styleguide">
@@ -14,12 +14,12 @@ export default ({ locals }) => (
 
 				<Rhythm size="small" deep="true">
 					<Heading level="2">Tags</Heading>
-					<ComponentList baseUrl="/styleguide/tags" components={locals.tags} />
+					<LinkList baseUrl="/styleguide/tags" components={locals.tags} />
 				</Rhythm>
 
 				<Rhythm size="small" deep="true">
 					<Heading level="2">Components</Heading>
-					<ComponentList baseUrl="/styleguide/components" components={locals.components} />
+					<LinkList baseUrl="/styleguide/components" components={locals.components} />
 				</Rhythm>
 			</Rhythm>
 	</Wrapper>
