8b8429a48dfc1fee90350d84d565819f1b509b1e
Kam Figy <kamsar@gmail.com>
2016-11-05 17:46:00 -0700

    Add comments to all build files as to what they do
    And who depends on them
    And how to use them
    
    ...based on my reading and reverse engineering of the build system, and being confused by it thereof. Hopefully this helps someone else later ;)

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,8 +1,13 @@
-const webpack = require('webpack');
-
-let build = require('./webpack.config');
-
+/* 
+  Configures webpack to build the site for production deployment.
+  
+  This file imports the debug configuration (webpack.config.js) and extends it to set production 
+  tasks such as minification to enabled. Thus, changes to the debug build also apply here unless
+  this configuration actively undoes them.
+*/
 const cssnano = require('cssnano');
+const webpack = require('webpack');
+let build = require('./webpack.config');
 
 // production specific configuration
 build.forEach((b, i) => {
@@ -10,6 +15,8 @@ build.forEach((b, i) => {
 });
 
 // add production flag to build environment
+// libraries can key off this to import versions without debug info
+// (e.g. react turns off warnings in the console and gets much smaller because of this)
 build.forEach((b, i) => {
 	build[i].plugins.unshift(
 	  new webpack.DefinePlugin({
