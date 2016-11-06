42e8725f8d8fe30b5421c9249eea112c46d10894
Kam Figy <kamsar@gmail.com>
2016-11-05 17:51:24 -0700

    Make watch kicker load up using configured source path

--- webpack-kicker.js
+++ webpack-kicker.js
@@ -31,7 +31,7 @@ const respawn = debounce(() => {
 
 const watcher =
 	chokidar
-		.watch('./source', { ignoreInitial: true })
+		.watch(config.directories.source, { ignoreInitial: true })
 		.on('ready', () => respawn())
 		.on('addDir', () => respawn())
 		.on('unlinkDir', () => respawn());
