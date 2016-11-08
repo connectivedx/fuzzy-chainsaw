c5a1f5212603e88a9c8b626f32b9941f18da8780
Kam Figy <kamsar@gmail.com>
2016-11-08 12:34:44 -0800

    Change watch to webpack-dev-server, with the kill-and-reload methodology already present

--- build/webpack-watch.js
+++ build/webpack-watch.js
@@ -1,35 +1,54 @@
 const chokidar = require('chokidar');
 const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
 
 const pkg = require('../package.json');
-const webpackErrorHandler = require('./webpack-errorhandler');
+
+const host = 'localhost';
+const port = 8080;
+let server;
 
 module.exports = config => {
-  let watcher;
 
-  const watch = changedPath => {
-    if(watcher && changedPath) {
-      // stop the old watcher
+  const watch = (changedPath) => {
+    if(server && changedPath) {
+      // stop the old dev server
       console.log('Restarting webpack watch due to change webpack will not see...')
       console.log(changedPath);
-      watcher.close();
+      server.close();
     }
 
-    // start the new watcher
-    watcher = webpack(config, (err, stats) => {
-      // note we're passing it a fake callback because we don't want watch to terminate on error :)
-      webpackErrorHandler(err, stats, { noexit: true }, function() {});
-    });
-  }
+    // start the new dev server
 
-  // enable watch mode on all webpack configs
-  config.forEach((b, i) => {
-    b.watch = true;
-  });
+    // want to try for HMR on the configs?
+    // See http://andrewhfarmer.com/webpack-hmr-tutorial/
+    // and https://github.com/ahfarmer/webpack-hmr-starter-dev-server-api
+    // but didn't work for me. Got errors about window not being defined
+    // after registering the HMR entry points, and the page markup was not updating
+
+    var compiler = webpack(config);
+
+    // see https://webpack.github.io/docs/webpack-dev-server.html#api for dev-server API options
+    server = new WebpackDevServer(compiler, {
+      contentBase: pkg.directories.output,
+      filename: "bundle.js",
+      publicPath: "/",
+      hot: false,
+      stats: {
+        colors: true,
+        chunks: false,
+        hash: false,
+        timings: false,
+        version: false
+      }
+    });
+
+    server.listen(port, host, () => { console.log(`webpack-dev-server is now listening on http://${host}:${port}`); });
+  }
 
   chokidar.watch(pkg.directories.source, { ignoreInitial: true })
     .on('ready', watch)
     .on('addDir', watch)
     .on('unlinkDir', watch)
-    .on('error', error => console.error('Webpack watch had an error: ', error));
+    .on('error', error => console.error('webpack-dev-server had an error: ', error));
 };
