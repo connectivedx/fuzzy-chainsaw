3591d7cd58b2c0a2221106b9d9c109820299e79b
Christoph Saxe <csaxe@connectivedx.com>
2016-11-09 21:49:50 -0800

    FormInput: basic input types

--- source/tags/FormInput/FormInput.jsx
+++ source/tags/FormInput/FormInput.jsx
@@ -1,30 +1,58 @@
 import React from 'react';
 import styles from './FormInput.css';
 
-export const FormInputText = ({
-	autocomplete,
+// "Text" types: text, search | url, telephone |
+const textType = (type, placeholder, {
+	className,
+	value,
+	autoComplete,
 	list,
-	maxlength,
-	minlength,
+	maxLength,
+	minLength,
 	name,
 	pattern,
 	size,
-	readonly,
-	required,
-	placeholder = 'Enter Text',
-	value = ''
-}) => (
-	let opts = {...autocomplete, list, maxlength, minlength, name, pattern, size, readonly, required};
+	readOnly,
+	required
+}) => {
+	let props = {type, value, className, placeholder, autoComplete, list, maxLength, minLength, name, pattern, size, readOnly, required};
 
-	<FormInput type="text" placeholder={placeholder} value={value} {...opts} />
-);
+	return ( <FormInput {...props} /> );
+};
 
-export default FormInput(obj) => {
-	let attrs = Object.assign({}, obj, {
-			className: "form-input form-input--" + (obj.type || 'text') + ' ' + (obj.className || ''),
-			id: obj.id || Date.now()
+export const FormInputText = (props) => {
+	return textType('text', (props.placeholder || 'Enter text'), props);
+};
+export const FormInputSearch = (props) => {
+	return textType('search', (props.placeholder || 'Search'), props);
+};
+export const FormInputUrl = (props) => {
+	return textType('url', (props.placeholder || 'Enter a valid URL'), props);
+};
+export const FormInputTel = (props) => {
+	return textType('tel', (props.placeholder || 'Enter a telephone number'), props);
+};
+export const FormInputEmail = (props) => {
+	return textType('email', (props.placeholder || 'Enter email'), props);
+};
+export const FormInputPassword = (props) => {
+	let scrubbed = Object.assign({}, props, {list: undefined});
+	
+	return textType('password', undefined, props);
+};
+export const FormInputHidden = (props) => {
+	return ( <FormInput type="hidden" value={props.value} /> );
+};
+
+export const FormInput = (props) => {
+	let attrs = Object.assign({}, props, {
+			className: "form-input form-input--" + (props.type || 'text') + ' ' + (props.className || ''),
+			id: props.id || Date.now(),
+			value: props.value || ''
 		});
 
 	return ( <input {...attrs} /> );
-}
+};
+
+export default FormInput;
 
--- source/tags/FormInput/FormInput.test.jsx
+++ source/tags/FormInput/FormInput.test.jsx
@@ -1,14 +1,91 @@
 import React from 'react';
-import FormInput from './FormInput.jsx';
+import {FormInput, FormInputText, FormInputSearch, FormInputUrl, FormInputTel, FormInputEmail, FormInputPassword, FormInputHidden} from './FormInput.jsx';
 
 export default [{
 	name: "default",
 	component: (
-		<FormInput type="text" placeholder="Foo!" />
+		<FormInput type="text" />
 	),
 	test(t, component) {
 		t.equal(component.is('input'), true, 'tag name');
 		t.equal(component.is('.form-input'), true, 'tag class');
+		t.end();
+	}
+},{
+	name: "text",
+	component: (
+		<FormInputText value="baaaaaLarrrGGGH!" />
+	),
+	test(t, component) {
+		t.equal(component.is('FormInput'), true, 'tag name');
+		// t.equal(component.is('.form-input'), true, 'tag class');
+		t.equal(component.is('[type="text"]'), true, 'input type');
+		t.end();
+	}
+},{
+	name: "search",
+	component: (
+		<FormInputSearch />
+	),
+	test(t, component) {
+		t.equal(component.is('FormInput'), true, 'tag name');
+		// t.equal(component.is('.form-input'), true, 'tag class');
+		t.equal(component.is('[type="search"]'), true, 'input type');
+		t.end();
+	}
+},{
+	name: "url",
+	component: (
+		<FormInputUrl />
+	),
+	test(t, component) {
+		t.equal(component.is('FormInput'), true, 'tag name');
+		// t.equal(component.is('.form-input'), true, 'tag class');
+		t.equal(component.is('[type="url"]'), true, 'input type');
+		t.end();
+	}
+},{
+	name: "tel",
+	component: (
+		<FormInputTel />
+	),
+	test(t, component) {
+		t.equal(component.is('FormInput'), true, 'tag name');
+		// t.equal(component.is('.form-input'), true, 'tag class');
+		t.equal(component.is('[type="tel"]'), true, 'input type');
+		t.end();
+	}
+},{
+	name: "email",
+	component: (
+		<FormInputEmail />
+	),
+	test(t, component) {
+		t.equal(component.is('FormInput'), true, 'tag name');
+		// t.equal(component.is('.form-input'), true, 'tag class');
+		t.equal(component.is('[type="email"]'), true, 'input type');
+		t.end();
+	}
+},{
+	name: "password",
+	component: (
+		<FormInputPassword />
+	),
+	test(t, component) {
+		t.equal(component.is('FormInput'), true, 'tag name');
+		// t.equal(component.is('.form-input'), true, 'tag class');
+		t.equal(component.is('[type="password"]'), true, 'input type');
+		t.end();
+	}
+},{
+	name: "hidden",
+	component: (
+		<FormInputHidden value="bananas" />
+	),
+	test(t, component) {
+		t.equal(component.is('FormInput'), true, 'tag name');
+		// t.equal(component.is('.form-input'), true, 'tag class');
+		t.equal(component.is('[type="hidden"]'), true, 'input type');
 		t.end();
 	}
 }];
