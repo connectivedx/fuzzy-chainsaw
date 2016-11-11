708a510d4c8b4e04042cd7afb1f7545cef6414bb
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-11-11 11:41:34 -0800

    an escaped console.log!

--- build/webpack-post-post-css-plugin.js
+++ build/webpack-post-post-css-plugin.js
@@ -3,7 +3,6 @@ const match = require('minimatch');
 
 function PostPostCSSPlugin(plugins) {
   this.postPostCssPlugins = plugins;
-  console.log(plugins);
 }
 
 PostPostCSSPlugin.prototype.apply = function(compiler) {
