a048e05b1c3d343e02fb104b54fadef77ec84908
Christoph Saxe <csaxe@connectivedx.com>
2016-11-17 15:55:57 -0800

    completes development of FormCheckgroup tag

--- source/tags/FormCheckgroup/FormCheckgroup.jsx
+++ source/tags/FormCheckgroup/FormCheckgroup.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import styles from './FormCheckgroup.css';
+import uniqueid from 'lodash.uniqueid';
+import FormFieldset from '../FormFieldset/FormFieldset';
+import FormCheck from '../FormCheck/FormCheck';
+
+const createOption = (setName, type, variant, {value = '', checkName, label = '', className = '', id = uniqueid('form-checkgroup__control_'), required}) => {
+	const name = checkName || setName;
+	const attrs = {name, type, value, variant, id, required};
+	return (
+		<div key={uniqueid().toString()} className={"form-checkgroup__item form-checkgroup__item--" + type}>
+			<FormCheck {...attrs} className={"form-checkgroup__control" + className} />
+			<label htmlFor={id} className={"form-checkgroup__label"}>{label}</label>
+		</div>
+	);
+};
 
 export default ({
 	className = '',
-	children
-}) => (
-	<div className={"form-checkgroup " + className}>
-		{children}
-	</div>
-)
+	checks = false,
+	type = 'checkbox',
+	variant = 'cssIcons',
+	name = uniqueid('form-checkgroup_'),
+	tagName,
+	legendTag,
+	legendText
+}) => {
+	const children = (Array.isArray(checks)) ? checks.map( (check)=>(createOption(name, type, variant, check)) ) : '';
+	const fieldsetProps = {tagName, legendTag, legendText};
+
+	return (
+		<FormFieldset className={`form-checkgroup form-checkgroup--${type} ${className}`} {...fieldsetProps}>
+			{children}
+		</FormFieldset>
+	);
+}
--- source/tags/FormCheckgroup/FormCheckgroup.test.jsx
+++ source/tags/FormCheckgroup/FormCheckgroup.test.jsx
@@ -1,17 +1,61 @@
 import React from 'react';
 import FormCheckgroup from './FormCheckgroup.jsx';
 
+const testOptions = [
+	{
+		label: 'first choice',
+		value: 'one'
+	},{
+		label: 'second choice',
+		value: 'two'
+	},{
+		label: 'third choice',
+		value: 'three'
+	}
+];
+const legendText = 'This is the legend';
+
 export default [{
 	name: "default",
 	component: (
-		<FormCheckgroup>
-			Hello World
-		</FormCheckgroup>
+		<FormCheckgroup checks={testOptions} />
 	),
 	test(t, component) {
-		t.equal(component.is('div'), true, 'tag name');
-		t.equal(component.is('.form-checkgroup'), true, 'tag class');
-		t.equal(component.text(), 'Hello World', 'text');
+		// t.equal(component.is('fieldset'), true, 'tag name');
+		// t.equal(component.is('.form-checkgroup'), true, 'tag class');
+		t.end();
+	}
+},{
+	name: "radios",
+	component: (
+		<FormCheckgroup legendText={legendText} type="radio" checks={testOptions} />
+	),
+	test(t, component) {
+		// t.equal(component.is('div'), true, 'tag name');
+		// t.equal(component.is('.form-checkgroup'), true, 'tag class');
+		// t.equal(component.text(), 'Hello World', 'text');
+		t.end();
+	}
+},{
+	name: "svg checkboxes",
+	component: (
+		<FormCheckgroup legendText={legendText} variant="svgIcons" checks={testOptions} />
+	),
+	test(t, component) {
+		// t.equal(component.is('div'), true, 'tag name');
+		// t.equal(component.is('.form-checkgroup'), true, 'tag class');
+		// t.equal(component.text(), 'Hello World', 'text');
+		t.end();
+	}
+},{
+	name: "svg radios",
+	component: (
+		<FormCheckgroup legendText={legendText} variant="svgIcons" type="radio" checks={testOptions} />
+	),
+	test(t, component) {
+		// t.equal(component.is('div'), true, 'tag name');
+		// t.equal(component.is('.form-checkgroup'), true, 'tag class');
+		// t.equal(component.text(), 'Hello World', 'text');
 		t.end();
 	}
 }];
