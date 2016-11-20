55989d1d8ddc472c11bcadcd47ae61ba3cc92b13
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-11-19 23:13:07 -0800

    make indexing more reusable

--- source/styleguide/components/Nav.jsx
+++ source/styleguide/components/Nav.jsx
@@ -2,43 +2,11 @@ import React from 'react';
 
 import Heading from '../../tags/Heading/Heading';
 import Rhythm from '../../tags/Rhythm/Rhythm';
-import LinkList from './LinkList';
-
-
-const path2LinkList = baseUrl => path => {
-  return ({
-    url: `${baseUrl}/${path}.html`,
-    content: path
-  });
-}
-
-export const Nav_List = ({ locals, listClassName }) => {
-  const tags =
-    locals.outputPaths
-      .filter(p => p.indexOf('/tags/') !== -1)
-      .map(p => p.substr(p.indexOf('/tags/') + '/tags/'.length))
-      .map(p => p.substr(0, p.length - '.html'.length));
-
-  const components =
-    locals.outputPaths
-      .filter(p => p.indexOf('/components/') !== -1)
-      .map(p => p.substr(p.indexOf('/components/') + '/components/'.length))
-      .map(p => p.substr(0, p.length - '.html'.length));
-
-  return (
-    <Rhythm size="large">
-      <Rhythm>
-        <Heading level="2">Tags</Heading>
-        <LinkList items={tags.map(path2LinkList('/styleguide/tags'))} className={listClassName} />
-      </Rhythm>
-
-      <Rhythm>
-        <Heading level="2">Components</Heading>
-        <LinkList items={components.map(path2LinkList('/styleguide/components'))} className={listClassName} />
-      </Rhythm>
-    </Rhythm>
-  );
-}
+import {
+  getTagsIndexData,
+  getComponentsIndexData,
+  Index
+} from './Index';
 
 export default ({
   locals
@@ -50,7 +18,17 @@ export default ({
     </a>
 
     <div className="sg-nav__container">
-      <Nav_List locals={locals} />
+      <Rhythm size="large">
+        <Rhythm>
+          <Heading level="2">Tags</Heading>
+          <Index items={getTagsIndexData(locals)} />
+        </Rhythm>
+
+        <Rhythm>
+          <Heading level="2">Components</Heading>
+          <Index items={getComponentsIndexData(locals)} />
+        </Rhythm>
+      </Rhythm>
     </div>
 
     <div className="sg-nav__cover"></div>
