881b756b088c2c270b9f11f7c8f1c95a90691014
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-11-19 23:13:28 -0800

    update pages to use auto indexer

--- source/pages/index.jsx
+++ source/pages/index.jsx
@@ -4,19 +4,33 @@ import PageRoot from '../tags/PageRoot/PageRoot';
 import Heading from '../tags/Heading/Heading';
 import Rhythm from '../tags/Rhythm/Rhythm';
 import Wrapper from '../tags/Wrapper/Wrapper';
-import { Nav_List as StyleguideList } from '../styleguide/components/Nav';
+import {
+  getTagsIndexData,
+  getComponentsIndexData,
+  getPagesIndexData,
+  Index
+} from '../styleguide/components/Index';
 
 export default ({ locals }) => (
 	<PageRoot title="Hello World">
 		<Wrapper>
 			<Rhythm size="large">
 				<Heading level="1">Hello World</Heading>
-        <StyleguideList locals={locals} listClassName="rhythm--small" />
 
-				<Rhythm size="small">
-					<Heading level="2">Pages</Heading>
-					<p>n/a</p>
-				</Rhythm>
+        <Rhythm>
+          <Heading level="2">Tags</Heading>
+          <Index items={getTagsIndexData(locals)} className="rhythm--small" />
+        </Rhythm>
+
+        <Rhythm>
+          <Heading level="2">Components</Heading>
+          <Index items={getComponentsIndexData(locals)} className="rhythm--small" />
+        </Rhythm>
+
+        <Rhythm>
+          <Heading level="2">Pages</Heading>
+          <Index items={getPagesIndexData(locals)} className="rhythm--small" />
+        </Rhythm>
 			</Rhythm>
 		</Wrapper>
 	</PageRoot>
--- source/pages/styleguide/index.jsx
+++ source/pages/styleguide/index.jsx
@@ -4,14 +4,27 @@ import PageRoot from '../../tags/PageRoot/PageRoot';
 import Rhythm from '../../tags/Rhythm/Rhythm';
 import Heading from '../../tags/Heading/Heading';
 import Wrapper from '../../tags/Wrapper/Wrapper';
-import { Nav_List as StyleguideList } from '../../styleguide/components/Nav';
+import {
+  getTagsIndexData,
+  getComponentsIndexData,
+  Index
+} from '../../styleguide/components/Index';
 
 export default ({ locals }) => (
 	<PageRoot title="Styleguide">
 		<Wrapper>
 			<Rhythm size="large">
 				<Heading level="1">Styleguide</Heading>
-        <StyleguideList locals={locals} listClassName="rhythm--small" />
+
+        <Rhythm>
+          <Heading level="2">Tags</Heading>
+          <Index items={getTagsIndexData(locals)} className="rhythm--small" />
+        </Rhythm>
+
+        <Rhythm>
+          <Heading level="2">Components</Heading>
+          <Index items={getComponentsIndexData(locals)} className="rhythm--small" />
+        </Rhythm>
 			</Rhythm>
   	</Wrapper>
 	</PageRoot>
