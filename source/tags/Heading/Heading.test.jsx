7d84f6ed25c51f7e1e0d8446a02c1f87b30b9d87
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-11-22 20:58:20 -0800

    100 tests!

--- source/tags/Heading/Heading.test.jsx
+++ source/tags/Heading/Heading.test.jsx
@@ -12,6 +12,7 @@ export default [{
 		t.equal(component.is('h1'), true, 'tag name');
 		t.equal(component.is('.heading'), true, 'tag class');
 		t.equal(component.is('.heading--h1'), true, 'variant class');
+    t.equal(component.is('.heading--default'), true, 'weight class');
 		t.equal(component.text(), 'Hello', 'text');
 		t.end();
 	}
@@ -26,6 +27,7 @@ export default [{
 		t.equal(component.is('h3'), true, 'tag name');
 		t.equal(component.is('.heading'), true, 'tag class');
 		t.equal(component.is('.heading--h3'), true, 'variant class');
+    t.equal(component.is('.heading--default'), true, 'weight class');
 		t.equal(component.text(), 'Wowie Zowie', 'text');
 		t.end();
 	}
@@ -40,6 +42,7 @@ export default [{
 		t.equal(component.is('h1'), true, 'tag name');
 		t.equal(component.is('.heading'), true, 'tag class');
 		t.equal(component.is('.heading--h1'), true, 'variant class');
+    t.equal(component.is('.heading--medium'), true, 'weight class');
 		t.equal(component.is('.super'), true, 'prop class');
 		t.equal(component.text(), 'Leg Shaking', 'text');
 		t.end();
@@ -54,7 +57,8 @@ export default [{
 	test(t, component) {
 		t.equal(component.is('div'), true, 'tag name');
 		t.equal(component.is('.heading'), true, 'tag class');
-		t.equal(component.is('.heading--default'), true, 'variant class');
+    t.equal(component.is('.heading--h1'), true, 'variant class');
+		t.equal(component.is('.heading--thin'), true, 'weight class');
 		t.equal(component.is('.duper'), true, 'prop class');
 		t.equal(component.text(), 'Back Breaking', 'text');
 		t.end();
