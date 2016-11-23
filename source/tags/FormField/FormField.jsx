31c31832ec0c100a3b32ebf313d00deedd1e1516
Christoph Saxe <christoph.saxe@gmail.com>
2016-11-22 22:49:14 -0800

    Completes FormField with sub-elements

--- source/tags/FormField/FormField.jsx
+++ source/tags/FormField/FormField.jsx
@@ -2,15 +2,76 @@ import React from 'react';
 import styles from './FormField.css';
 
 
-const FormField = ({
-	id = uniqueid('form-input_'),
+export const FormField_Label = ({
+	tagName = 'div',
 	className = '',
-	children
-}) => (
-	<div className={`form-input-field form-input-field--${variant} ${className}`}>
-		{children}
-	</div>
-);
+	align,
+	children,
+	...attr
+}) => {
+	const Tag = tagName;
+
+	if (align !== undefined) {
+		className = `form-field__label--${align} ${className}`;
+	}
+
+	return (
+		<Tag className={`form-field__label ${className}`} {...attr}>
+			{children}
+		</Tag>
+	);
+};
+
+export const FormField_Error = ({
+	tagName = 'div',
+	className = '',
+	align,
+	children,
+	...attr
+}) => {
+	const Tag = tagName;
+
+	if (align !== undefined) {
+		className = `form-field__error--${align} ${className}`;
+	}
+
+	return (
+		<Tag className={`form-field__error ${className}`} {...attr}>
+			{children}
+		</Tag>
+	);
+};
+
+export const FormField_Control = ({
+	tagName = 'div',
+	className = '',
+	children,
+	...attr
+}) => {
+	const Tag = tagName;
+
+	return (
+		<Tag className={`form-field__control ${className}`} {...attr}>
+			{children}
+		</Tag>
+	);
+};
+
+export const FormField = ({
+	tagName = 'div',
+	variant = 'default',
+	className = '',
+	children,
+	...attr
+}) => {
+	const Tag = tagName;
+
+	return (
+		<Tag className={`form-field form-field--${variant} ${className}`} {...attr}>
+			{children}
+		</Tag>
+	);
+};
 
 FormField.propTypes = {
 	variant: React.PropTypes.string,
--- source/tags/FormField/FormField.test.jsx
+++ source/tags/FormField/FormField.test.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import FormField from './FormField.jsx';
+import {FormField, FormField_Label, FormField_Control, FormField_Error} from './FormField.jsx';
+import FormLabel from '../FormLabel/FormLabel.jsx';
+import FormInput from '../FormInput/FormInput.jsx';
 
 export default [{
 	name: "default",
 	component: (
 		<FormField>
-			Hello World
+			<FormField_Label>
+				<FormLabel>Label</FormLabel>
+			</FormField_Label>
+			<FormField_Control>
+				<FormInput />
+			</FormField_Control>
+			<FormField_Error>
+				<FormLabel variant="error">Error</FormLabel>
+			</FormField_Error>
 		</FormField>
 	),
 	test(t, component) {
 		t.equal(component.is('div'), true, 'tag name');
 		t.equal(component.is('.form-field'), true, 'tag class');
-		t.equal(component.text(), 'Hello World', 'text');
+		// t.equal(component.text(), 'Hello World', 'text');
 		t.end();
 	}
 }];
