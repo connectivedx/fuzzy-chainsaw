fa9be3c9da6139b9536a0bd3034615cf5ae56408
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-12-08 19:21:44 -0800

    wrapper spaces

--- source/tags/Wrapper/Wrapper.jsx
+++ source/tags/Wrapper/Wrapper.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import styles from './Wrapper.css';
 
 const Wrapper = ({
-	size = 'default',
-	className = '',
-	children
+  size = 'default',
+  className = '',
+  children
 }) => {
-	return (
-		<div className={`wrapper wrapper--${size} ${className}`}>
-			{children}
-		</div>
-	)
+  return (
+    <div className={`wrapper wrapper--${size} ${className}`}>
+      {children}
+    </div>
+  )
 };
 
 Wrapper.propTypes = {
-	size: React.PropTypes.string,
-	className: React.PropTypes.string
+  size: React.PropTypes.string,
+  className: React.PropTypes.string
 };
 
 export default Wrapper;
