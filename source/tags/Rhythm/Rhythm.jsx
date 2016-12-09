e9d56535c53c96d7e4ee1a092c02d8c82bbc18d1
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-12-08 19:14:56 -0800

    proptypes

--- source/tags/Rhythm/Rhythm.jsx
+++ source/tags/Rhythm/Rhythm.jsx
@@ -20,9 +20,13 @@ const Rhythm = ({
 };
 
 Rhythm.propTypes = {
+  tagName: React.PropTypes.string,
+  className: React.PropTypes.string,
   size: React.PropTypes.string,
-  deep: React.PropTypes.string,
-  className: React.PropTypes.string
+  deep: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.boolean
+  ])
 };
 
 export default Rhythm;
