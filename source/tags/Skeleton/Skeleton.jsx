276fb2c5467b61e72002a3b5d3be71c57f4eb4a4
Dan Manchester <dan@elseloop.com>
2016-12-30 12:20:30 -0800

    Removes the X-UA-Compatible meta tag
    …after researching, rethinking, and learning better.

--- source/tags/Skeleton/Skeleton.jsx
+++ source/tags/Skeleton/Skeleton.jsx
@@ -19,7 +19,6 @@ export const Skeleton_Head = ({
   <head>
     <meta charSet="utf-8" />
     <title>{title}</title>
-    <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/assets/styles.css" />
     {children}
