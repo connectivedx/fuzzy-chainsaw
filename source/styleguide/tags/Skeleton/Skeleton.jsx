4f4b7c1a1dd541ed66b4e433eebc70fa4adba485
Dan Manchester <dan@elseloop.com>
2017-01-23 09:48:44 -0800

    Adds viewport tag to style guide <Skeleton>

--- source/styleguide/tags/Skeleton/Skeleton.jsx
+++ source/styleguide/tags/Skeleton/Skeleton.jsx
@@ -10,10 +10,9 @@ export default ({
 }) => (
   <html>
     <head>
-      <title>{title}</title>
-
       <meta charSet="utf-8" />
-
+      <title>{title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="/assets/styles.css" />
       <link rel="stylesheet" href="/assets/styleguide.css" />
     </head>
