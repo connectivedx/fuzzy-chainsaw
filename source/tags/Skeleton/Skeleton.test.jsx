2155356cb3979e9d6daf9ed340024d1c38bb26f8
Evan Krambuhl <ekrambuhl@connectivedx.com>
2017-02-01 22:35:59 -0800

    update media tag to use Image tag

--- source/tags/Skeleton/Skeleton.test.jsx
+++ source/tags/Skeleton/Skeleton.test.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import Skeleton from './Skeleton';
-import rancheriaFallsImage from './assets/rancheria-falls.jpg';
 
 export default [{
   name: 'default',
   component: (
     <Skeleton title="Super Test Page">
       Oh, Hello
-    </Skeleton>
-  )
-}, {
-  name: 'responsive image test',
-  component: (
-    <Skeleton title="big responsive image test page">
-      <img src={rancheriaFallsImage.src} srcSet={rancheriaFallsImage.srcSet} width="100%" />
     </Skeleton>
   )
 }];
