ee3b8f42adcc631845edfec888a91088b7dfad4c
Evan Krambuhl <ekrambuhl@connectivedx.com>
2017-03-06 00:49:15 -0800

    update css

--- source/styles.jsx
+++ source/styles.jsx
@@ -3,14 +3,14 @@ require.context('Vars/', true, /\.css$/);
 
 // require any components that
 // need to preserve ordering
-require('Tags/Skeleton/Skeleton.css');
+require('Tags/Root/Root.css');
 
-// require module for richtext first
 require('Tags/Rhythm/Rhythm.css');
 require('Tags/Heading/Heading.css');
 require('Tags/Button/Button.css');
+require('Tags/List/List.css');
 require('Tags/RichText/RichText.css');
 
 // require the rest of the components
-require.context("Tags/", true, /\.css$/);
-require.context("Components/", true, /\.css$/);
+require.context('Tags/', true, /\.css$/);
+require.context('Components/', true, /\.css$/);
