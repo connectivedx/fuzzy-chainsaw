21c4c0ee8a5a8d94acc517b5654fa9e95a21ae50
Kam Figy <kamsar@gmail.com>
2017-03-08 11:14:21 -0800

    Adds a file hash to the iconset svg

--- source/tags/Icon/IconSet.js
+++ source/tags/Icon/IconSet.js
@@ -1,2 +1,2 @@
-const __svg__ = { path: './assets/**/*.svg', name: '/assets/svgs/iconset.svg' };
+const __svg__ = { path: './assets/**/*.svg', name: '/assets/svgs/iconset-[hash].svg' };
 require('webpack-svgstore-plugin/src/helpers/svgxhr')(__svg__);
