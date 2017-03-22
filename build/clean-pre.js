35ee11c04de58a8869679b7451a6719f6a20a268
Evan Krambuhl <ekrambuhl@connectivedx.com>
2017-03-22 01:21:15 -0700

    makes pre clean a bit more targeted

--- build/clean-pre.js
+++ build/clean-pre.js
@@ -1,9 +1,18 @@
 const del = require('del');
 const path = require('path');
 const pkgpath = require('packpath');
+
 const pkg = require(path.resolve(pkgpath.self(), 'package.json'));
 const dirs = pkg.directories;
 
-module.exports = () => {
-  return del(path.resolve(pkgpath.self(), dirs.dest), { force: true })
-};
+module.exports = () => (
+  del([
+    `${path.resolve(pkgpath.self(), dirs.dest)}/**/*.html`,
+    `${path.resolve(pkgpath.self(), dirs.dest)}/assets/*.{js,js.map,css,css.map}`,
+    `${path.resolve(pkgpath.self(), dirs.dest)}/assets/images/**`,
+    `${path.resolve(pkgpath.self(), dirs.dest)}/assets/fonts/**`,
+    `${path.resolve(pkgpath.self(), dirs.dest)}/assets/svgs/**`
+  ], {
+    force: true
+  })
+);
