d602ae9a5f5f8b16a5bbe91edbc5bc1c8086f2ca
Evan Krambuhl <thekrambuhl@gmail.com>
2017-04-21 14:21:46 -0700

    add link component

---
+++ source/tags/Link/Link.example.jsx
@@ -0,0 +1,13 @@
+import Link from './Link';
+
+export default [{
+  name: 'Default styling',
+  component: (
+    <Link href="#slime">Lorem ipsum</Link>
+  )
+}, {
+  name: 'Cta styling',
+  component: (
+    <Link href="#slime" variant="cta">Click me</Link>
+  )
+}];
