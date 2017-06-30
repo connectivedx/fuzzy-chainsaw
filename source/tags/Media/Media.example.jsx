9c2ab802eb5f18a67d2efed3e2cd6d8b35462b6d
Evan Krambuhl <thekrambuhl@gmail.com>
2017-06-30 15:24:31 -0700

    media subcomponents

--- source/tags/Media/Media.example.jsx
+++ source/tags/Media/Media.example.jsx
@@ -3,110 +3,110 @@ import Heading from 'Tags/Heading/Heading';
 import Rhythm from 'Tags/Rhythm/Rhythm';
 
 import randySavage from './assets/randy-savage.jpg';
-import { Media, Media_Figure, Media_Body } from './Media';
+import { Media, Media__figure, Media__body } from './Media';
 
 export default [{
   name: 'media to the left',
   component: (
     <Media>
-      <Media_Figure>
+      <Media__figure>
         <Image src={randySavage} alt="Randy Savage" />
-      </Media_Figure>
+      </Media__figure>
 
-      <Media_Body>
+      <Media__body>
         <Rhythm size="small">
           <Heading level="h3">Randy Savage Buddy</Heading>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
         </Rhythm>
-      </Media_Body>
+      </Media__body>
     </Media>
   )
 }, {
   name: 'media to the right',
   component: (
     <Media>
-      <Media_Body>
+      <Media__body>
         <Rhythm size="small">
           <Heading level="h3">Randy Savage Buddy</Heading>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
         </Rhythm>
-      </Media_Body>
+      </Media__body>
 
-      <Media_Figure>
+      <Media__figure>
         <Image src={randySavage} alt="Randy Savage" />
-      </Media_Figure>
+      </Media__figure>
     </Media>
   )
 }, {
   name: 'media align middle',
   component: (
     <Media align="middle">
-      <Media_Figure>
+      <Media__figure>
         <Image src={randySavage} alt="Randy Savage" />
-      </Media_Figure>
+      </Media__figure>
 
-      <Media_Body>
+      <Media__body>
         <Rhythm size="small">
           <Heading level="h3">Randy Savage Buddy</Heading>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
         </Rhythm>
-      </Media_Body>
+      </Media__body>
     </Media>
   )
 }, {
   name: 'media align bottom',
   component: (
     <Media align="bottom">
-      <Media_Figure>
+      <Media__figure>
         <Image src={randySavage} alt="Randy Savage" />
-      </Media_Figure>
+      </Media__figure>
 
-      <Media_Body>
+      <Media__body>
         <Rhythm size="small">
           <Heading level="h3">Randy Savage Buddy</Heading>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
         </Rhythm>
-      </Media_Body>
+      </Media__body>
     </Media>
   )
 }, {
   name: 'media to each side, different alignment',
   component: (
     <Media>
-      <Media_Figure>
+      <Media__figure>
         <Image src={randySavage} alt="Randy Savage" />
-      </Media_Figure>
+      </Media__figure>
 
-      <Media_Body>
+      <Media__body>
         <Rhythm size="small">
           <Heading level="h3">Randy Savage Buddy</Heading>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facilis accusantium dolorum deserunt rem ex voluptas impedit doloribus facere, velit, possimus! Aliquid amet, corrupti cumque quae perferendis eos recusandae, nam non dicta velit rem enim vitae! Facere, aliquid inventore reiciendis.</p>
         </Rhythm>
-      </Media_Body>
+      </Media__body>
 
-      <Media_Figure align="bottom">
+      <Media__figure align="bottom">
         <Image src={randySavage} alt="Randy Savage" />
-      </Media_Figure>
+      </Media__figure>
     </Media>
   )
 }, {
   name: 'media nested',
   component: (
     <Media>
-      <Media_Figure>
+      <Media__figure>
         <Image src={randySavage} alt="Randy Savage" />
-      </Media_Figure>
+      </Media__figure>
 
-      <Media_Figure>
+      <Media__figure>
         <Image src={randySavage} alt="Randy Savage" />
-      </Media_Figure>
+      </Media__figure>
 
-      <Media_Body>
+      <Media__body>
         <Rhythm size="large">
           <Rhythm size="small">
             <Heading level="h3">Randy Savage Buddy</Heading>
@@ -114,37 +114,37 @@ export default [{
           </Rhythm>
 
           <Media>
-            <Media_Figure>
+            <Media__figure>
               <Image src={randySavage} alt="Randy Savage" />
-            </Media_Figure>
+            </Media__figure>
 
-            <Media_Body>
+            <Media__body>
               <Rhythm size="small">
                 <Heading level="h3">Lorem ipsum dolor sit.</Heading>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quisquam sequi odit hic adipisci, ad consequatur quasi id aspernatur debitis saepe voluptatum veritatis consequuntur quas porro dolor molestiae error iste iusto voluptate nisi atque tempore! Beatae quod debitis modi soluta!</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quisquam sequi odit hic adipisci, ad consequatur quasi id aspernatur debitis saepe voluptatum veritatis consequuntur quas porro dolor molestiae error iste iusto voluptate nisi atque tempore! Beatae quod debitis modi soluta!</p>
               </Rhythm>
-            </Media_Body>
+            </Media__body>
           </Media>
 
           <Media>
-            <Media_Body>
+            <Media__body>
               <Rhythm size="small">
                 <Heading level="h3">Lorem ipsum dolor sit.</Heading>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quisquam sequi odit hic adipisci, ad consequatur quasi id aspernatur debitis saepe voluptatum veritatis consequuntur quas porro dolor molestiae error iste iusto voluptate nisi atque tempore! Beatae quod debitis modi soluta!</p>
               </Rhythm>
-            </Media_Body>
+            </Media__body>
 
-            <Media_Figure>
+            <Media__figure>
               <Image src={randySavage} alt="Randy Savage" />
-            </Media_Figure>
+            </Media__figure>
 
-            <Media_Figure align="bottom">
+            <Media__figure align="bottom">
               <Image src={randySavage} alt="Randy Savage" />
-            </Media_Figure>
+            </Media__figure>
           </Media>
         </Rhythm>
-      </Media_Body>
+      </Media__body>
     </Media>
   )
 }];
--- source/tags/Media/Media.test.jsx
+++ source/tags/Media/Media.test.jsx
@@ -1,31 +1,31 @@
 import test from 'tape';
 import { shallow } from 'enzyme';
 
-import { Media, Media_Figure, Media_Body } from './Media';
+import { Media, Media__figure, Media__body } from './Media';
 
 
 test('<Media>', (t) => {
   const component = shallow(
     <Media>
-      <Media_Figure>slime</Media_Figure>
-      <Media_Body>slime</Media_Body>
+      <Media__figure>slime</Media__figure>
+      <Media__body>slime</Media__body>
     </Media>
   );
   t.ok(component.is('div'), 'tag name');
   t.ok(component.is('.Media'), 'tag class');
-  t.equal(component.text(), '<Media_Figure /><Media_Body />', 'text');
+  t.equal(component.text(), '<Media__figure /><Media__body />', 'text');
   t.end();
 });
 
 test('<Media align="bottom">', (t) => {
   const component = shallow(
     <Media align="bottom">
-      <Media_Body>slime</Media_Body>
-      <Media_Figure>slime</Media_Figure>
+      <Media__body>slime</Media__body>
+      <Media__figure>slime</Media__figure>
     </Media>
   );
   t.ok(component.is('div'), 'tag name');
   t.ok(component.is('.Media'), 'tag class');
-  t.equal(component.text(), '<Media_Body /><Media_Figure />', 'text');
+  t.equal(component.text(), '<Media__body /><Media__figure />', 'text');
   t.end();
 });
