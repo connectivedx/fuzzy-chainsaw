9abdccb2beb3059493fd6a55cbde9badfc690c5b
Evan Krambuhl <thekrambuhl@gmail.com>
2017-07-03 00:04:16 -0700

    use .render().text() to get reasonable testing output

--- source/tags/Media/Media.test.jsx
+++ source/tags/Media/Media.test.jsx
@@ -7,26 +7,26 @@ import { Media, Media__figure, Media__body } from './Media';
 test('<Media>', (t) => {
   const component = shallow(
     <Media>
-      <Media__figure>slime</Media__figure>
-      <Media__body>slime</Media__body>
+      <Media__figure>abc</Media__figure>
+      <Media__body>123</Media__body>
     </Media>
   );
   t.ok(component.is('div'), 'tag name');
   t.ok(component.hasClass('Media'), 'tag class');
-  t.equal(component.text(), '<Media__figure /><Media__body />', 'text');
+  t.equal(component.render().text(), 'abc123', 'text');
   t.end();
 });
 
 test('<Media align="bottom">', (t) => {
   const component = shallow(
     <Media align="bottom">
-      <Media__body>slime</Media__body>
-      <Media__figure>slime</Media__figure>
+      <Media__body>123</Media__body>
+      <Media__figure>abc</Media__figure>
     </Media>
   );
   t.ok(component.is('div'), 'tag name');
   t.ok(component.hasClass('Media'), 'tag class');
   t.ok(component.hasClass('Media--bottom'), 'variant class');
-  t.equal(component.text(), '<Media__body /><Media__figure />', 'text');
+  t.equal(component.render().text(), '123abc', 'text');
   t.end();
 });
