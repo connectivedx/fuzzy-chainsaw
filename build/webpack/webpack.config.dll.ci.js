4a0c4d3d60f39a5e76ab79759add1b27a43971a8
Evan Krambuhl <thekrambuhl@gmail.com>
2017-07-03 01:57:17 -0700

    add non production ci builds

---
+++ build/webpack/webpack.config.dll.ci.js
@@ -0,0 +1,13 @@
+const webpackMerge = require('webpack-merge');
+const dllWorkflow = require('./workflow/dll');
+const { source } = require('../lib/path-helpers');
+
+const { dlls } = require(source('fc-config')); // eslint-disable-line
+
+module.exports = webpackMerge(dllWorkflow, {
+  entry: {
+    vendor: [
+      'react'
+    ]
+  }
+});
--- build/webpack/webpack.config.dll.js
+++ build/webpack/webpack.config.dll.js
@@ -5,15 +5,15 @@ const dllWorkflow = require('./workflow/dll');
 module.exports = webpackMerge(dllWorkflow, {
   entry: {
     vendor: [
-      'react',
-      'react-dom',
-      'prop-types'
+      'react'
     ],
     styleguide: [
       'react-dom/server',
       'react-modal',
       'pretty',
       'prismjs',
+      'react-dom',
+      'prop-types',
       'react-element-to-jsx-string',
       'slugify',
       'lodash.startcase'
