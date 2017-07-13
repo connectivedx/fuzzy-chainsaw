505201ddfdec36a01434c9abeba97aea670f219b
Evan Krambuhl <thekrambuhl@gmail.com>
2017-07-13 14:17:57 -0700

    comment bundle

--- source/bundle.jsx
+++ source/bundle.jsx
@@ -1,7 +1,12 @@
+// import css before other
+// files to preserve ordering
 import '@source/bundle.css';
+
+// import FC deps
 import '@tags/Icon/IconSet';
 import '@lib/theme-handler';
 import '@lib/offline-runtime';
 
 
+// program
 console.log('Main Bundle!');
