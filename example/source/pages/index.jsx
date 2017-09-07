9502fa903d87749d75ed1813f08d6ca051ca361c
Evan Krambuhl <thekrambuhl@gmail.com>
2017-09-06 21:43:15 -0700

    add padding to input page

--- example/source/pages/index.jsx
+++ example/source/pages/index.jsx
@@ -1,24 +1,26 @@
 import {
   Heading,
   Rhythm,
+  Padding,
   Wrapper
 } from '@tags';
 
 import tableOfContents from 'fuzzy-chainsaw-toc';
 import * as archive from '@source/archive';
 
-const toc = tableOfContents(archive);
 
 const page = () => (
-  <Wrapper size="wide">
-    <Rhythm size="large">
-      <Rhythm size="small">
-        <Heading level="h1">Fuzzy Chainsaw</Heading>
-        <Heading level="h4">Front End Style Guide</Heading>
-      </Rhythm>
+  <Wrapper size="large">
+    <Padding size="medium-large">
+      <Rhythm size="large">
+        <Rhythm size="small">
+          <Heading level="h1">Fuzzy Chainsaw</Heading>
+          <Heading level="h4">Front End Style Guide</Heading>
+        </Rhythm>
 
-      <div className="RichText" dangerouslySetInnerHTML={{ __html: toc }} />
-    </Rhythm>
+        <div className="RichText" dangerouslySetInnerHTML={{ __html: tableOfContents(archive) }} />
+      </Rhythm>
+    </Padding>
   </Wrapper>
 );
 
