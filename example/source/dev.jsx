2607345079ce4008ec4e986be877500db6254964
Evan Krambuhl <thekrambuhl@gmail.com>
2017-09-07 23:19:57 -0700

    wait for page load before dev startup

--- example/source/dev.jsx
+++ example/source/dev.jsx
@@ -1,8 +1,11 @@
 import startDevMode from 'fuzzy-chainsaw-bundle/helpers/start-dev-mode';
 import framework from 'fuzzy-chainsaw-framework-react';
 
-startDevMode({
-  appRoot: document.querySelector('.root'),
-  framework,
-  archive: global.archive
+
+document.addEventListener('DOMContentLoaded', () => {
+  startDevMode({
+    appRoot: document.querySelector('.root'),
+    framework,
+    archive: global.archive
+  });
 });
