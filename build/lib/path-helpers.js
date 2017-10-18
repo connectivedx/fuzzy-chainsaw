4816fa087d47b69f5847fdbc1f5623dad5b6a6e2
Brian Staruk <brian@staruk.me>
2017-10-18 07:50:56 -0400

    use .includes instead of .indexOf

--- build/lib/path-helpers.js
+++ build/lib/path-helpers.js
@@ -11,10 +11,11 @@ const { directories, baseUrl: packageBaseUrl } = require(path.resolve(pkgpath.se
 const source = (...paths) => path.resolve(pkgpath.self(), directories.source, ...paths);
 const dest = (...paths) => path.resolve(pkgpath.self(), directories.dest, ...paths);
 
-// dev always serves from root (http://localhost:8080),
+// dev always serves from root (localhost:8080),
 // so only use baseUrl if task is not dev-related
 const devTasks = ['dev', 'build:dev', 'test:dev'];
-const baseUrl = devTasks.indexOf(process.env.npm_lifecycle_event) ? '/' : process.env.BASE_URL || packageBaseUrl || '/';
+const baseUrl = devTasks.includes(process.env.npm_lifecycle_event) ? '/' : process.env.BASE_URL || packageBaseUrl || '/';
+
 
 // This takes a simple object of entry:filepath pairs
 // and runs each filepath through the source() helper
