b991c08bde23d9a8b4a0e5bcf3aff73cd56057c9
Conner Wells <conner.c.wells@gmail.com>
2017-12-07 10:29:35 -0800

    adds 'colors.jsx' page to display the Swatches

---
+++ source/pages/styleguide/colors.jsx
@@ -0,0 +1,16 @@
+import Heading from '@tags/Heading/Heading';
+import Wrapper from '@tags/Wrapper/Wrapper';
+import SgColorSwatch from '@sg-tags/SgColorSwatch/SgColorSwatch';
+
+const page = () => (
+  <Wrapper size="wide">
+    <Heading>Fuzzy Chainsaw Colors</Heading>
+    <br />
+    <SgColorSwatch />
+  </Wrapper>
+);
+
+page.pageTitle = 'StyleGuide | Colors';
+page.pageType = 'index';
+
+export default page;
