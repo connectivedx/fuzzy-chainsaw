715dfd6ff2759efa2cb7ec04f7642ec60c6a4f9d
Conner Wells <conner.c.wells@gmail.com>
2017-12-11 12:15:34 -0800

    freshens up the 'colors' page with more documentation

--- source/pages/styleguide/colors.jsx
+++ source/pages/styleguide/colors.jsx
@@ -1,10 +1,20 @@
 import Heading from '@tags/Heading/Heading';
+import Rhythm from '@tags/Rhythm/Rhythm';
 import Wrapper from '@tags/Wrapper/Wrapper';
 import SgColorSwatch from '@sg-components/SgColorSwatch/SgColorSwatch';
 
 const page = () => (
   <Wrapper size="wide">
-    <Heading>Fuzzy Chainsaw Colors</Heading>
+    <Rhythm>
+      <Heading>Fuzzy Chainsaw Colors</Heading>
+      <Heading level="h3">We are generating Project Colors! Neat!</Heading>
+      <p>Colors are cool! (And so are you!) You can see all of the color variables defined in &#39;variables/colors.css&#39; here!</p>
+      <Heading level="h2">Accessibility</Heading>
+      <p>
+        We run 2 Accessibility tests based on the <a href="https://www.w3.org/TR/WCAG20/">WCAG 2.0 Standards</a> for web content.
+        For each font color in the project, we test to the AA Standard for &#34;normal&#34; and &#34;large&#34; font size categories.
+      </p>
+    </Rhythm>
     <br />
     <SgColorSwatch />
   </Wrapper>
