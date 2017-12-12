38cd63f948ea512016dbd51ac369ae24aa011522
Conner Wells <conner.c.wells@gmail.com>
2017-12-12 09:31:05 -0800

    bye bye old nav

--- source/styleguide/styleguide.jsx
+++ source/styleguide/styleguide.jsx
@@ -1,26 +1,18 @@
 // import css first
 import styles from '@styleguide/styleguide.css'; // eslint-disable-line
 
-// npm modules
-import Dom from 'react-dom';
-
 // component modules
-import SgNav from '@sg-components/SgNav/SgNav';
 import SgExample from '@sg-components/SgExample/SgExample.Container';
 import SgStyleguide from '@sg-components/SgStyleguide/SgStyleguide.Container';
 
 
 const ui = {
-  nav: document.querySelector('.SgNav'),
   styleguide: document.querySelector('.SgStyleguide'),
   examples: Array.prototype.slice.call(document.querySelectorAll('.SgExample'))
 };
 
 
 const init = () => {
-  // Table of contents
-  Dom.render(<SgNav />, ui.nav);
-
   // Readme toggle
   SgStyleguide(ui.styleguide);
 
