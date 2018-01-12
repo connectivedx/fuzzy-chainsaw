f38ffef42d95e21fe746e75397fd4244d04c0ca2
dolsen <dolsen@connectivedx.com>
2018-01-12 14:54:03 -0800

    Fixes #292

--- build/webpack-watch-factory.js
+++ build/webpack-watch-factory.js
@@ -9,8 +9,8 @@ const gulp = require('gulp');
 const watch = require('gulp-watch');
 const { source } = require('./lib/path-helpers');
 
-module.exports = () => {
-	watch([ source('**/**') ], () => {
+module.exports = () => () => {
+	watch([ source('elements/components/**') ], () => {
 		gulp.start('webpack:watch:sequence');
 	}); 
 };
