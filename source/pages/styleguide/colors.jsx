a65ea2f39b59c81789661a2374249284dc473452
Petra Gregorová <pgregorova@connectivedx.com>
2018-02-09 11:00:19 -0500

    updated verbiage how we test for color contrast

--- source/pages/styleguide/colors.jsx
+++ source/pages/styleguide/colors.jsx
@@ -26,8 +26,10 @@ const page = () => (
           <Rhythm>
             <Heading>Fuzzy Chainsaw Colors</Heading>
             <p>
-              We run two accessibility tests based on the <a href="https://www.w3.org/TR/WCAG20/">WCAG 2.0 Standards</a> for web content.
-              For each font color defined within the project, we test color contrast to the AA Standard for the defined “normal” (14pt/18px) and “large” (18pt/24px) font sizes.
+              We run 2 Accessibility tests based on the <Link href="http://www.w3.org/TR/2008/REC-WCAG20-20081211/#visual-audio-contrast-contrast">WCAG 2.0 contrast ratio formula</Link>. The WCAG 2.0 formula differentiates between text smaller than 18pt/24px, text larger than 18pt (or text that is bold and larger than 14pt/19px).
+            </p>
+            <p>For AA compliance, text should have a ratio of at least 4.5:1 (larger text, at least 3:1). For AAA compliance, text should have a ratio of at least 7:1 (larger text, at least 4.5:1).
+              For each font color in the project, we test to the AA Standard for &#34;normal&#34; and &#34;large&#34; font size categories.
             </p>
           </Rhythm>
           <br />
