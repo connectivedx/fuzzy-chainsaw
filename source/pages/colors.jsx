34ace6faa1442a4dcffae2d9ab7cbc5ec49607f6
drolsen <dolsen@connectivedx.com>
2018-03-16 16:46:12 -0700

    Changes Link usage back to just simple anchor

--- source/pages/colors.jsx
+++ source/pages/colors.jsx
@@ -26,7 +26,7 @@ const page = () => (
           <Rhythm>
             <Heading>Fuzzy Chainsaw Colors</Heading>
             <p>
-              We run 2 Accessibility tests based on the <Link href="http://www.w3.org/TR/2008/REC-WCAG20-20081211/#visual-audio-contrast-contrast">WCAG 2.0 contrast ratio formula</Link>. The WCAG 2.0 formula differentiates between text smaller than 18pt/24px, text larger than 18pt (or text that is bold and larger than 14pt/19px).
+              We run 2 Accessibility tests based on the <a href="http://www.w3.org/TR/2008/REC-WCAG20-20081211/#visual-audio-contrast-contrast">WCAG 2.0 contrast ratio formula</a>. The WCAG 2.0 formula differentiates between text smaller than 18pt/24px, text larger than 18pt (or text that is bold and larger than 14pt/19px).
             </p>
             <p>For AA compliance, text should have a ratio of at least 4.5:1 (larger text, at least 3:1). For AAA compliance, text should have a ratio of at least 7:1 (larger text, at least 4.5:1).
               For each font color in the project, we test to the AA Standard for &#34;normal&#34; and &#34;large&#34; font size categories.
