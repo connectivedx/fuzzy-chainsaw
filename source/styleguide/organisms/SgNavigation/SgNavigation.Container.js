f49582269a9897c22ad72408ca84ce048738de02
drolsen <dolsen@connectivedx.com>
2018-03-19 14:08:47 -0700

    Fixes table of contents navigation active link

--- source/styleguide/organisms/SgNavigation/SgNavigation.Container.js
+++ source/styleguide/organisms/SgNavigation/SgNavigation.Container.js
@@ -2,13 +2,15 @@ import select from 'dom-select';
 
 export default (el) => {
   const menuItems = select.all('.SgFileIndex__name', el);
-  const activeItem = menuItems.filter((item, index) => {
-    if (item.href.indexOf(window.location.pathname) !== -1) {
-      return index + 1;
+
+  const activeItem = menuItems.filter((item) => {
+    if (item.href.indexOf(window.location.pathname) >= 0) {
+      return item;
     }
     return false;
   });
-  if (activeItem.length) {
+
+  if (activeItem.length && activeItem.length === 1) {
     activeItem[0].classList.add('is-active');
   }
 };
